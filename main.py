f332c31a0ea9655d654d31b2f5a0dccd14ba9d46
manasarao54747@gmail.com <manasarao54747@gmail.com>
2023-04-09 18:20:09 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player=Player()
+car_manager=CarManager()
+scoreboard=Scoreboard()
+screen.listen()
+screen.onkey(player.up,"Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #detect collision
+    for car in car_manager.all_cars:
+        if car.distance(player)<20:
+            game_is_on=False
+            scoreboard.game_over()
+
+    #detect when the player reaches the other side
+    if player.ycor() == 290:
+        player.reset_position()
+        car_manager.move_cars()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
